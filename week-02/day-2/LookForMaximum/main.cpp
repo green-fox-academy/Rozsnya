f5bce4934b0d64c0faa96b2ba793b65444ddba38
Peter Rozsnyai <peter.rozsnyai@hotmail.com>
2019-01-22 16:30:07 +0100

    update 8. exercise

--- week-02/day-2/LookForMaximum/main.cpp
+++ week-02/day-2/LookForMaximum/main.cpp
@@ -7,23 +7,23 @@ int main()
     // and than asks for numbers till the user fills the array
     // It should print out the biggest number in the given array and the memory address of it
 
-    int Largest = 0;
-    int* p_large = nullptr;
+
     int arraySize;
     std::cout << "Give me the array size" << std::endl;
     std::cin >> arraySize;
     int myArray[arraySize];
+    int* p_large = &myArray[0];
 
     for(int i = 0; i < arraySize; i++){
         std::cout << "Give me numbers" << std::endl;
         std::cin >> myArray[i];
     }
     for(int i = 0; i < arraySize; i++){
-        if (myArray[i] > Largest){
-            Largest = myArray[i];
+        if (myArray[i] > *p_large){
+            p_large = &myArray[i];
         }
     }
-    p_large = &Largest;
+
     std::cout << "The biggest number is: " << *p_large << " " << "and it's adress is: " << p_large << std::endl;
 
 
