fe554bef6bd3444c0ef33d4da416353d727894b3
Peter Rozsnyai <peter.rozsnyai@hotmail.com>
2019-01-23 14:22:13 +0100

    add 1.core exercise

---
+++ week-02/day-3/CopyFIle/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
+// It should take the filenames as parameters
+// It should return a boolean that shows if the copy was successful
+
+bool myFunc(std::string fileOne, std::string fileTwo){
+    std::string text = "";
+    std::ifstream myFile1(fileOne);
+    std::ofstream myFile2(fileTwo);
+
+    while(myFile1 >> text){
+        myFile2 << text << std::endl;
+    }
+    return true;
+}
+
+int main() {
+
+    std::cout << myFunc("original.txt", "copy.txt") << std::endl;
+
+    return 0;
+}
