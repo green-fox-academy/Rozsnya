551309a1910e119832d3d64840237ebaad0f6f58
Peter Rozsnyai <peter.rozsnyai@hotmail.com>
2019-01-30 10:21:28 +0100

    add 9. exercise

---
+++ week-03/day-3/Dominoes/domino.h
@@ -0,0 +1,24 @@
+//
+// Created by peter on 2019. 01. 29..
+//
+
+#ifndef DOMINOES_DOMINO_H
+#define DOMINOES_DOMINO_H
+
+#include <iostream>
+#include <utility>
+
+class Domino
+{
+public:
+    Domino(int valueA, int valueB);
+
+    std::pair<int,int> getValues();
+    std::string toString();
+
+private:
+    std::pair<int,int> _values;
+};
+
+
+#endif //DOMINOES_DOMINO_H
